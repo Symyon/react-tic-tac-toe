70393fe19923c63470714886fe3eece3ac9ac139
simion.nacu@gmail.com <simion.nacu@gmail.com>
2019-02-13 14:16:39 -0500

    App, added a basic structure with basic components

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import Board from '../Board';
+import Score from '../Score';
+import Button from '../Button';
+
 import './index.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.undo = this.undo.bind(this);
+    this.restart = this.restart.bind(this);
+  }
+
+  undo () {
+
+  }
+
+  restart () {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue' }}>
+        <div>
+          <div style={{ display: 'flex', flex: 1, flexDirection: 'row', width: '100%' }}>
+            <Score text = 'X score'/>
+            <Score text = 'O score'/>
+          </div>
+          <Board />
+          <div style={{ display: 'flex', flex: 1, flexDirection: 'row' }}>
+            <Button text ='Undo' onClick = {this.undo}/>
+            <Button text='Restart' onClick = {this.restart}/>
+          </div>
+        </div>
       </div>
     );
   }
