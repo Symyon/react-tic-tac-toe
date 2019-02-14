a206036112ec84c264926f61c0695595f90674fc
simion.nacu@gmail.com <simion.nacu@gmail.com>
2019-02-13 23:16:12 -0500

    Updates

---
+++ precache-manifest.0f37bd680b177b67dff5b4970992174d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36488e54f820f64d5ed14ad504c86ad7",
+    "url": "/react-tic-tac-toe/static/media/wall.36488e54.jpg"
+  },
+  {
+    "revision": "2a998508be6b0622f6128be063ac51eb",
+    "url": "/react-tic-tac-toe/static/media/o.2a998508.png"
+  },
+  {
+    "revision": "7d1b9009599586221ce9",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.7d1b9009.js"
+  },
+  {
+    "revision": "dd1720107ad1f26561dc",
+    "url": "/react-tic-tac-toe/static/js/main.dd172010.chunk.js"
+  },
+  {
+    "revision": "36176ca670f1999ae6f0",
+    "url": "/react-tic-tac-toe/static/js/2.36176ca6.chunk.js"
+  },
+  {
+    "revision": "dd1720107ad1f26561dc",
+    "url": "/react-tic-tac-toe/static/css/main.9c9914ea.chunk.css"
+  },
+  {
+    "revision": "7c8a1c389029f82db77f8f9ba1569d64",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
