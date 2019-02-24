a0f015386676e5a9ee89dfd9ea9cdb50b7f4ea35
simion.nacu@gmail.com <simion.nacu@gmail.com>
2019-02-24 13:41:05 -0500

    Test Score component render for different activity values

--- src/components/Score/test.js
+++ src/components/Score/test.js
@@ -13,4 +13,12 @@ describe('Score', () => {
         const component = shallow(<Score value={5} />);
         expect(component).toMatchSnapshot();
     });
+
+    it('render corectly when passing activity flag', () => {
+        const activeScore = shallow(<Score active = {true} />);
+        expect(activeScore).toMatchSnapshot();
+        
+        const passiveScore = shallow(<Score active = {false} />);
+        expect(passiveScore).toMatchSnapshot();       
+    });
 });
