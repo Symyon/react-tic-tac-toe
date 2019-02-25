6f9df457807c814d4b10f8b105715ecb56b50acb
simion.nacu@gmail.com <simion.nacu@gmail.com>
2019-02-25 06:22:44 -0500

    Snapshots for Cell render without props, with mark as empyt/cross/nought

---
+++ src/components/Cell/test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Cell from './';
+
+import { NOUGHT, CROSS, EMPTY } from '../../constants';
+
+describe('Cell', () => {
+    it('render correctly without props', () => {
+        const component = shallow(<Cell />);
+        expect(component).toMatchSnapshot();
+    })
+
+    it('render correctly empty', () => {
+        const component = shallow(<Cell mark={EMPTY} />);
+        expect(component).toMatchSnapshot();
+    })
+
+    it('render correctly with cross', () => {
+        const component = shallow(<Cell mark={CROSS} />);
+        expect(component).toMatchSnapshot();
+    })
+
+    it('render correctly with nought', () => {
+        const component = shallow(<Cell mark={NOUGHT} />);
+        expect(component).toMatchSnapshot();
+    })
+});
