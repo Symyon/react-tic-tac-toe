bdfe6fe089a21995a1c2bb9dc73cb938f5a75ae1
simion.nacu@gmail.com <simion.nacu@gmail.com>
2019-02-25 07:26:33 -0500

    Test for board overlay render when game finishes for winners or draw cases

--- src/components/Board/test.js
+++ src/components/Board/test.js
@@ -2,9 +2,26 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Board from './';
 
+import { NOUGHT, CROSS, EMPTY, DRAW } from '../../constants';
+
 describe('Board', () => {
     it('render correctly without props', () => {
         const component = shallow(<Board />);
+        expect(component).toMatchSnapshot();
+    })
+
+    it('renders vorrectly the overlay when a draw', () => {
+        const component = shallow(<Board winner={DRAW} />);
+        expect(component).toMatchSnapshot();
+    })
+
+    it('renders vorrectly the overlay when noughts are winners', () => {
+        const component = shallow(<Board winner={NOUGHT} />);
+        expect(component).toMatchSnapshot();
+    })
+
+    it('renders vorrectly the overlay when crosses are winners', () => {
+        const component = shallow(<Board winner={CROSS} />);
         expect(component).toMatchSnapshot();
     })
 });
