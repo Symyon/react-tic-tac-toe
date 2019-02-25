f70849bbeeb14c93047f9791b340e549a7b3c3cf
simion.nacu@gmail.com <simion.nacu@gmail.com>
2019-02-25 11:44:16 -0500

    Added tests for checking if the App component will render also a Board

--- src/components/App/test.js
+++ src/components/App/test.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import App from '.';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it('renders correctly', () => {
-  const component = shallow(<App />);
-  expect(component).toMatchSnapshot();
+  it('renders correctly', () => {
+    const component = shallow(<App />);
+    expect(component).toMatchSnapshot();
+  });
+
+  
+
+  it('renders 1 Board component', () => {
+    const component = render(<App />);
+    expect(component.find('div.Board').length).toEqual(1);
+  })
 });
